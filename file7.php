e65137d232d33f3bf3eef68861417bfb6af93ca4
vcdsdgbg9v-del <vcdsdgbg9v@privaterelay.appleid.com>
2026-09-21 12:07:56 +0400

    Create     file7.php
    Signed-off-by: vcdsdgbg9v-del <vcdsdgbg9v@privaterelay.appleid.com>

---
+++ file7.php
@@ -0,0 +1,14 @@
+class Account():
+    base_url=''
+    data_auth=''
+    idToken=''
+
+class Url_send():
+    SaveCarsPartially='/SaveCarsPartially5/'
+    GetPlayerRecords='/GetPlayerRecords2/'
+    deleteAccountOne='/deleteAccountOne/'
+    TestGetAllCars='/TestGetAllCars/'
+    SavePlayerRecordsPartially='/SavePlayerRecordsPartially5/'
+
+class Noneheaders():
+    pass
